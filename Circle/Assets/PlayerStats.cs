49f5f42ea2e4d5122c7ad64c1ffd91546a65be1d
GoogFB <glui7983@gmail.com>
2022-02-25 10:04:28 -0800

    refer to branch name
    I made the dummy and gave him an hp stat. we will possibly lose hp. no hitbox yet

---
+++ Circle/Assets/PlayerStats.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namspace SG
+{
+
+public class PlayerStats : MonoBehaviour
+{
+
+	public int healthLevel = 100;
+	public int maxHealth;
+	public int currentHealth
+
+    void Start()
+    {
+        
+    
+        
+}
+}
+}
