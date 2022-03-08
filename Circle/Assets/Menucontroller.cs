20292dd0f47e00493f4f1a999bf86e58eb37ddbd
LaBambaBot <mn746220@gmail.com>
2022-03-08 10:08:59 -0800

    Menu Buttons working
    Everything Is done just working on all settings for the Menu

--- Circle/Assets/Menucontroller.cs
+++ Circle/Assets/Menucontroller.cs
@@ -2,19 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.sceneManagement;
+using UnityEngine.SceneManagement;
 
-public class menucon : MonoBehaviour
+public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Levels To Load")]
+    public string _newGameLevel;
+    private string levelToLoad;
+    [SerializeField] private GameObject noSavedGameDialog = null;
+
+
+    public void NewGameDialogYes()
     {
-        
+        SceneManager.LoadScene(_newGameLevel);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGameDialogYes()
     {
-        
+        if (PlayerPrefs.HasKey("SavedLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            noSavedGameDialog.SetActive(true);
+        }
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
     }
 }
