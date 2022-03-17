ab0e4981ea7305abbd23b1b4f88c71a7021eb36c
GoogFB <glui7983@gmail.com>
2022-03-17 08:55:40 -0700

    new code, new issues
    added new code. im about 88% certain it works, just 2 errors. I can taste victory.

---
+++ Circle/Assets/DummyHealth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyHealth : MonoBehaviour
+{
+    public int DummyMaxHealth = 100;
+    public int DummyCurrentHealth;
+    public HealthBar DummyHealthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DummyCurrentHealth = DummyMaxHealth;
+        DummyHealthBar.SetMaxHealth(DummyMaxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            DummyTakeDamage(25);
+        }
+    }
+    public void DummyTakeDamage(int damage)
+    {
+        DummyCurrentHealth -= damage;
+        DummyHealthBar.SetHealth(DummyCurrentHealth);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "john")
+        {
+            DummyTakeDamage(25);
+
+        }
+    }
+}
