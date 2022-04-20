2bfb37dbd5e47dddd0e86e938be246451b9aa758
LaBambaBot <mn746220@gmail.com>
2022-04-20 10:04:58 -0700

    Finish Volume code to change the volume

--- Circle/Assets/Options.cs
+++ Circle/Assets/Options.cs
@@ -1,6 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMpro
 
-public class NewBehaviourScript : MonoBehaviour
+public class MenuController : MonoBehaviour
+{
+    [Header("Volume Setting")]
+    [SerializeField] private Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
 
+    [SerializeField] private GameObject comfirmationPrompt = null;
+    public string _newGameLevel;
+    private string levelToLoad;
+    [SerializeField] private GameObject noSavedGameDialog = null;
+
+
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene(_newGameLevel);
+    }
+
+    public void LoadGameDialogYes()
+    {
+        if (PlayerPrefs.HasKey("SavedLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            noSavedGameDialog.SetActive(true);
+        }
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", Audiolistener.volume);
+        //Show prompt
+    }   StartCoroutine(ConfirmationBox());
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.setActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.setActive(false);
+    }
+}
