ac2c967013c400bea943ed93f7df08d35d6e2370
tony1629 <anthonyl-19@hotmail.com>
2022-05-05 10:14:00 -0700

    end of class 5/5/22

--- Circle/Assets/Scripts/WeaponHolderSlot.cs
+++ Circle/Assets/Scripts/WeaponHolderSlot.cs
@@ -32,27 +32,30 @@ namespace AL
         {
             UnloadWeaponAndDestroy();
 
-            if(weaponItem == null)
+            if (weaponItem == null)
             {
                 UnloadWeapon();
                 return;
             }
 
-            GameObject model = InstantLate(weaponItem.modelPrefab) as GameObject;
-            if(model != null)
+            GameObject model = Instantiate(weaponItem.modelPrefab) as GameObject;
+            if (model != null)
             {
-                model.transform.parent = parentOverride;
-            }
-            else
-            {
-                model.transform.parent = transform;
+                if(parentOverride != null)
+                {
+                    model.transform.parent = parentOverride;
+                }
+                else
+                {
+                    model.transform.parent = transform;
+                }
+
+                model.transform.localPosition = Vector3.zero;
+                model.transform.localRotation = Quaternion.identity;
+                model.transform.localScale = Vector3.one;
             }
 
-            model.transform.localPosition = Vector3.zero;
-            model.transform.localRotation = Quaternion.identify;
-            model.transform.localScale = Vector3.one;
+            currentWeaponModel = model;
         }
-
-        currentWeaponModel = model;
     }
 }
