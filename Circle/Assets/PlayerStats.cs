0d6eccb772251d85767e0088a6a70fddb8bd78c5
tony1629 <anthonyl-19@hotmail.com>
2022-05-11 10:06:22 -0700

    player stats script WIP

--- Circle/Assets/PlayerStats.cs
+++ Circle/Assets/PlayerStats.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour
+namespace AL
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerStats : MonoBehaviour
     {
         
-    }
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
