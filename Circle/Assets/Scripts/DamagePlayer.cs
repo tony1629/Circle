ca7fa1ab9d29170dde5386cc33a446e2cc927648
tony1629 <anthonyl-19@hotmail.com>
2022-05-13 10:07:00 -0700

    player stats added, damage player script WIP

---
+++ Circle/Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AL
+{
+    public class DamagePlayer : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+
+        }
+    }
+}
---
+++ Circle/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AL
+{
+    public class PlayerStats : MonoBehaviour
+    {
+        public int healthLevel = 10;
+        public int maxHealth;
+        public int currentHealth;
+
+        public HealthBar healthbar;
+        
+        void Start()
+        {
+            maxHealth = SetMaxHealthFromHealthLevel();
+            currentHealth = maxHealth;
+            healthbar.SetMaxHealth(maxHealth);
+        }
+
+        private int SetMaxHealthFromHealthLevel()
+        {
+            maxHealth = healthLevel * 10;
+            return maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            currentHealth = currentHealth - damage;
+
+            healthbar.SetHealth(currentHealth);
+        }
+    }
+}
