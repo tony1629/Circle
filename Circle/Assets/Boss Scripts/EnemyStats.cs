44c3cab0cf522358e0f3262fe13413cc5025f797
3dg4rl1 <lopezedgar2931@gmail.com>
2022-05-24 10:09:54 -0700

    Boss Event
    video at 7 minutes

--- Circle/Assets/Boss Scripts/EnemyStats.cs
+++ Circle/Assets/Boss Scripts/EnemyStats.cs
@@ -7,17 +7,52 @@ namespace SG
     public class EnemyStats : MonoBehaviour
     {
         EnemyAnimatorManager enemyAnimatorManager;
+        EnemyBossManager enemyBossManager;
         public UIBossHealthBar bossHealthBar;
+        public bool isBoss;
+
 
         private void Awake()
         {
             enemyAnimatorManager = GetComponentInChildren<EnemyAnimatorManager>();
+            enemyBossManager = GetComponent<EnemyBossManager>();
+            maxHealth = SetMaxHealthFromHealthLevel();
+            currentHealth = maxHealth;
         }
         void Start()
         {
-            //maxHealth = SetMaxHealthFromHealthLevel();
-            //currentHealth = maxHealth;
+           if (!isBoss)
+            {
+                bossHealthBar.SetBossMaxHealth(maxHealth);
+            }
             
+        }
+        private int SetMaxHealthFromHealthLevel()
+        {
+
+        }
+        public void TakeDamageNoAnimation(int damage)
+        {
+
+        }
+        public override void TakeDamage(int damage, string damageAnimation = "Damage_01")
+        {
+            base.TakeDamage(damage, damageAnimation = "Damage_01");
+            if (!isBoss)
+            {
+                bossHealthBar.SetHealth(currentHealth);
+            }
+            else
+            {
+                enemyBossManager.
+            }
+            
+            enemyAnimatorManager.PlayTargetAnimation(damageAnimation, true);
+
+            if(currentHealth <= 0)
+            {
+                HandleDeath();
+            }
         }
     }
 }
