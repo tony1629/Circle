5e0c77500a1a683c0e4fc193d7dd32779fc7b276
3dg4rl1 <lopezedgar2931@gmail.com>
2022-06-02 09:59:51 -0700

    boss event
    ep 32 8:45

--- Circle/Assets/Boss Scripts/EnemyManager.cs
+++ Circle/Assets/Boss Scripts/EnemyManager.cs
@@ -8,6 +8,9 @@ namespace EL
     {
         EnemyLocomotionManager enemyLocomotionManager;
         public bool isPerformingAction;
+
+        public EnemyAttackAction[] enemyAttacks;
+        public EnemyAttackAction currentAttack;
         [Header("A.I Settings")]
         public float detectionRadius = 20;
         public float maximumDetectionAngle = 50;
@@ -46,6 +49,49 @@ namespace EL
         private void GetNewAttack()
         {
             Vector3 targetsDirection = enemyLocomotionManager.currentTarget.transform.position - transform.position;
+            float viewableAngle = Vector3.Angle(targetsDirection, transform.forward);
+            enemyLocomotionManager.distanceFromTarget = Vector3.Distance(enemyLocomotionManager.currentTarget.transform.position, transform.position);
+
+            int maxScore = 0;
+            for( int i=0; i< enemyAttacks.Length ; i++)
+            {
+                EnemyAttackAction enemyAttackAction = enemyAttacks[i];
+
+                if(enemyLocomotionManager.distanceFromTarget <= enemyAttackAction.maximumDistanceNeededToAttack
+                    && enemyLocomotionManager.distanceFromTarget >= enemyAttackAction.minimumDistanceNeededToAttack)
+                {
+                    if(viewableAngle <= enemyAttackAction.maximumAttackAngle
+                        && viewableAngle >= enemyAttackAction.minimumAttackAngle)
+                    {
+                        maxScore += enemyAttackAction.attackScore;
+                    }
+                }
+            }
+            int randomValue = Random.Range(0, maxScore);
+            int temporaryScore = 0;
+
+            for(int i =0; i < enemyAttacks.Length; i++)
+            {
+                EnemyAttackAction enemyAttackAction = enemyAttacks[i];
+
+                if (enemyLocomotionManager.distanceFromTarget <= enemyAttackAction.maximumDistanceNeededToAttack
+                    && enemyLocomotionManager.distanceFromTarget >= enemyAttackAction.minimumDistanceNeededToAttack)
+                {
+                    if (viewableAngle <= enemyAttackAction.maximumAttackAngle
+                        && viewableAngle >= enemyAttackAction.minimumAttackAngle)
+                    {
+                        if (currentAttack != null)
+                            return;
+
+                        temporaryScore += enemyAttackAction.attackScore;
+
+                        if(temporaryScore > randomValue)
+                        {
+                            currentAttack = enemyAttackAction;
+                        }
+                    }
+                }
+            }
         }
         #endregion
 
